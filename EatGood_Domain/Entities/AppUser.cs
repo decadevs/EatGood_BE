d1f269ba35716be24252ab69dbae0612463b4e4d
Ifeanyi Mojekwu <107749742+Ifeanyi-M@users.noreply.github.com>
2024-03-24 10:33:43 +0100

    commit

--- EatGood_Domain/Entities/AppUser.cs
+++ EatGood_Domain/Entities/AppUser.cs
@@ -1,17 +1,18 @@
 ﻿
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace EatGood_Domain.Entities
 {
-    public class AppUser: IdentityUser
+    public class AppUser : IdentityUser
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public Address? Address { get; set; }
+        public string Address { get; set; }
         public bool IsDeleted { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
         public DateTime DateModified { get; set; }
-        public IEnumerable<Order>? Orders { get; set; }
+        //public IEnumerable<Order>? Orders { get; set; }
     }
 }
--- EatGood_Domain/Entities/Cart.cs
+++ EatGood_Domain/Entities/Cart.cs
@@ -2,21 +2,13 @@
 {
     public class Cart: BaseEntity
     {
-        public List<CartItem> Items { get; set; }
+        public List<FoodItem> Items { get; set; }
         public decimal TotalPrice { get; set; }
         public decimal Tax { get; set; }
         public decimal Subtotal { get; set; }
         public decimal Discount { get; set; }
         public ShippingDetails ShippingInfo { get; set; }
 
-        public Cart()
-        {
-            Items = new List<CartItem>();
-            TotalPrice = 0m;
-            Tax = 0m;
-            Subtotal = 0m;
-            Discount = 0m;
-            ShippingInfo = new ShippingDetails();
-        }
+        
     }
 }
